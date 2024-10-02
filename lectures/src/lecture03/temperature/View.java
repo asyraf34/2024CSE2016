49495be481abbf077d469642d67b7925887d335f
asyraf34 <asyraf67@hanyang.ac.kr>
2024-10-02 11:52:28 +0900

    View.getTemperature Completion

--- lectures/src/lecture03/temperature/View.java
+++ lectures/src/lecture03/temperature/View.java
@@ -1,8 +1,11 @@
 package lecture03.temperature;
 
+import javax.swing.*;
 public class View {
     double getTemperature() {
-        return 0.0;
+        String input = JOptionPane.showInputDialog(null,"Input temperature in celcius: ");
+        int c = Integer.parseInt(input);
+        return c;
     }
 
     void showTemperature(double c, double f) {
